7a3c0e60e17a143d5a3ced080b52a4fafd436531
Mateusz Uzdowski <mateusz@silverstripe.com>
2015-04-22 16:19:28 +1200

    New build script based on stojg/sera.

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -x
 
 BUILDDIR=./build
+LATEST_TAG=`git describe --abbrev=0 --tags`
+
+rm -rf ${BUILDDIR} && mkdir -p ${BUILDDIR}
 
 for dir in kinesis*; do
 	if [ -d $dir ]; then
 		echo "Building $dir"
 		cd $dir
-		$GOPATH/bin/gox -output="../$BUILDDIR/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch "linux/amd64 darwin/amd64"
+		$GOPATH/bin/gox -output="../$BUILDDIR/{{.Dir}}_${LATEST_TAG}_{{.OS}}_{{.Arch}}" -osarch "linux/amd64 darwin/amd64"
 		cd ..
 	fi
 done
